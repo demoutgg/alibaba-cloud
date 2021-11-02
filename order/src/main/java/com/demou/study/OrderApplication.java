028e24421c44276d981fa9bef319a2c40c11fae5
KF1910 <654487197@qq.com>
2021-11-03 00:15:42 +0800

    Initial commit

---
+++ order/src/main/java/com/demou/study/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.demou.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author：lyx
+ * @date: 2021/11/3 12:04 上午
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
---
+++ stock/src/main/java/study/StockApplication.java
@@ -0,0 +1,12 @@
+package study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class StockApplication {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(StockApplication.class,args);
+    }
+}
