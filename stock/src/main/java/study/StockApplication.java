d4b0f621d0a6e5fa59998f3282c8b3f1f405217f
KF1910 <654487197@qq.com>
2021-11-03 00:39:44 +0800

    init

--- stock/src/main/java/study/StockApplication.java
+++ stock/src/main/java/study/StockApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+/**
+ * @author：lyx
+ * @date: 2021/11/3 12:04 上午
+ * @version: 1.0
+ */
 @SpringBootApplication
 public class StockApplication {
     public static void main(String[] args) {
